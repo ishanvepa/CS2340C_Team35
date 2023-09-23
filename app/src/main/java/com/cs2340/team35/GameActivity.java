d28d48efcd8547fa38e6f7cd9efab182b3c254d6
Subhajit Das <subhajitdasw@gmail.com>
2023-09-23 11:41:03 -0400

    This commit adds GameActivity and WelcomeActivity to the repo

---
+++ app/src/main/java/com/cs2340/team35/GameActivity.java
@@ -0,0 +1,14 @@
+package com.cs2340.team35;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
