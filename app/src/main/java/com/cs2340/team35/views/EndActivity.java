3818af5e2dc7188a6800799f2ee99a9dc6898201
Nhat Le <nhatle@Nhats-MacBook-Pro-2.local>
2023-10-10 03:30:27 -0400

    Adding leaderboard and recent score and fix leaderboard button

--- app/src/main/java/com/cs2340/team35/views/EndActivity.java
+++ app/src/main/java/com/cs2340/team35/views/EndActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.cs2340.team35.R;
+import com.cs2340.team35.viewmodels.GameState;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class EndActivity extends AppCompatActivity {
 
@@ -22,5 +25,28 @@ public class EndActivity extends AppCompatActivity {
                     WelcomeActivity.class);
             startActivity(intent);
         });
+
+        TextView score = findViewById(R.id.Score);
+        score.setText("Recent Score");
+
+        TextView leaderBoard1 = findViewById(R.id.Leaderboard1);
+        TextView leaderBoard2 = findViewById(R.id.Leaderboard2);
+        TextView leaderBoard3 = findViewById(R.id.Leaderboard3);
+        TextView leaderBoard4 = findViewById(R.id.Leaderboard4);
+        TextView leaderBoard5 = findViewById(R.id.Leaderboard5);
+        TextView leaderBoard6 = findViewById(R.id.Leaderboard6);
+        TextView leaderBoard7 = findViewById(R.id.Leaderboard7);
+
+        leaderBoard1.setText("1");
+        leaderBoard2.setText("2");
+        leaderBoard3.setText("3");
+        leaderBoard4.setText("4");
+        leaderBoard5.setText("5");
+        leaderBoard6.setText("6");
+        leaderBoard7.setText("7");
+
+
+
     }
+
 }
