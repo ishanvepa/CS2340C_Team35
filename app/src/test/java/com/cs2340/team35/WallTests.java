c08fac30ee2be5f34147b9b644f3aed75a416479
ishanvepa <ishanvepa171@gmail.com>
2023-10-22 15:46:14 -0400

    this commit preps some test cases for the implementation of wall configuration and collisions.

---
+++ app/src/test/java/com/cs2340/team35/WallTests.java
@@ -0,0 +1,28 @@
+package com.cs2340.team35;
+import static org.junit.Assert.assertEquals;
+
+import com.cs2340.team35.models.GameModel;
+//import com.cs2340.team35.models.WallModel;
+import com.cs2340.team35.views.GameActivity;
+
+import org.junit.Test;
+
+public class WallTests {
+    @Test
+    public void TestWallConfiguration() {
+        //create a list of walls
+        //test to see if their params are correct using getters
+
+
+    }
+
+    @Test
+    public void TestWallCollisions(){
+        //create a wall
+        //collide character with it
+        //run isCollision
+
+
+    }
+
+}
