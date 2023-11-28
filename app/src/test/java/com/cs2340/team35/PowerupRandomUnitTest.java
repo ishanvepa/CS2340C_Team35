ffff2529ccda69a7c3ade5ad41fe6fdf2260fa3b
Nhat Le <nhatle@lawn-128-61-79-41.lawn.gatech.edu>
2023-11-27 19:52:33 -0500

    unit test for powerup randomnize spawn

--- app/src/test/java/com/cs2340/team35/PowerupRandomUnitTest.java
+++ app/src/test/java/com/cs2340/team35/PowerupRandomUnitTest.java
@@ -21,4 +21,26 @@ public class PowerupRandomUnitTest {
         }
         Assert.assertTrue(inRange);
     }
+
+    @Test
+    public void powerUpRandomLevel2() {
+        GameModel gameModel = GameModel.getInstance();
+        int randomX = gameModel.randomXinScreen(131, 907);
+        int randomY = gameModel.randomYinScreen(606, 1056);
+        PowerupBase testPowerupBase1 = new PowerupBase(false, randomX, randomY, "size1", "size");
+        boolean inRange1 = false;
+        if ((testPowerupBase1.getX() >= 131 && testPowerupBase1.getX() <= 907) && (testPowerupBase1.getY() >= 606 && testPowerupBase1.getY() <= 1056)) {
+            inRange1 = true;
+        }
+        Assert.assertTrue(inRange1);
+
+        int randomXx = gameModel.randomXinScreen(131, 907);
+        int randomYy = gameModel.randomYinScreen(606, 1056);
+        PowerupBase testPowerupBase2 = new PowerupBase(false, randomXx, randomYy, "speed1", "speed");
+        boolean inRange2 = false;
+        if ((testPowerupBase1.getX() >= 131 && testPowerupBase1.getX() <= 907) && (testPowerupBase1.getY() >= 606 && testPowerupBase1.getY() <= 1056)) {
+            inRange2 = true;
+        }
+        Assert.assertTrue(inRange2);
+    }
 }
